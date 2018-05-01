174c645594bc97e89dba909b2f818f496e72f4a7
Óscar Nájera <hello@oscarnajera.com>
2018-05-01 18:29:34 +0200

    fix syntax in install file

--- installs.sh
+++ installs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-apt-get update && apt install -y fonts-font-awesome
+apt-get update && apt-get install -y fonts-font-awesome
 wget https://github.com/gohugoio/hugo/releases/download/v0.39/hugo_0.39_Linux-64bit.deb
 dpkg -i hugo*.deb
 
@@ -11,7 +11,7 @@ hugo version
 echo "Install altacv"
 git clone https://github.com/Titan-C/AltaCV.git /root/texmf/tex/latex/AltaCV
 echo "Install fontawesome for latex"
-fontdir = /root/texmf/tex/latex/fontawesome/
+fontdir=/root/texmf/tex/latex/fontawesome/
 mkdir -p $fontdir
 
 wget -P $fontdir http://mirrors.ctan.org/fonts/fontawesome/tex/fontawesome.sty
