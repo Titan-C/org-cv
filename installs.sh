e8542bbed64dbfbe136a2b6b91eb9e7b1f8f1c50
Óscar Nájera <hello@oscarnajera.com>
2018-05-01 22:32:19 +0200

    fontawesome for latex should be in the docker container

--- installs.sh
+++ installs.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-apt-get update && apt-get install -y fonts-font-awesome
 wget https://github.com/gohugoio/hugo/releases/download/v0.39/hugo_0.39_Linux-64bit.deb
 dpkg -i hugo*.deb
 
@@ -15,10 +14,3 @@ unzip -j sections.zip -d $latexdir/AltaCV
 echo "Install moderncv"
 wget https://github.com/Titan-C/moderncv/archive/master.zip
 unzip -j master.zip -d $latexdir/moderncv
-echo "Install fontawesome for latex"
-fontdir=$latexdir/fontawesome/
-mkdir -p $fontdir
-
-wget -P $fontdir http://mirrors.ctan.org/fonts/fontawesome/tex/fontawesome.sty
-wget -P $fontdir http://mirrors.ctan.org/fonts/fontawesome/tex/fontawesomesymbols-generic.tex
-wget -P $fontdir http://mirrors.ctan.org/fonts/fontawesome/tex/fontawesomesymbols-pdftex.tex
